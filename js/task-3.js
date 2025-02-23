3800b85b22eb86e453accfa8be643057ba755b00
Tetiana Taratula <ttaratula@gmail.com>
2025-02-23 16:12:10 +0000

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,10 @@
+// Напиши скрипт, який під час набору тексту в інпуті input#name-input (подія input) підставляє його поточне значення в span#name-output як ім’я для привітання. Обов’язково очищай значення в інпуті по краях від пробілів . Якщо інпут порожній або містить лише пробіли, то замість імені у спан має підставлятися рядок "Anonymous".
+
+
+const inputEl = document.querySelector("#name-input");
+const outputEl = document.querySelector("#name-output");
+
+inputEl.addEventListener("input", () => {
+  const trimmedValue = inputEl.value.trim(); 
+  outputEl.textContent = trimmedValue ? trimmedValue : "Anonymous"; 
+});
---
+++ js/task-5.js
@@ -0,0 +1,20 @@
+// Напиши скрипт, який змінює колір фону елемента <body> через інлайн-стиль по кліку на button.change-color і задає це значення кольору текстовим вмістом для span.color.
+// Зверни увагу, що функція getRandomHexColor() повертає колір у hex-форматі, в той час як колір фону на <body> буде у форматі rgb. Це нормально й не потребує якихось правок.
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const changeColorBtn = document.querySelector(".change-color");
+const colorSpan = document.querySelector(".color");
+const body = document.body;
+
+changeColorBtn.addEventListener("click", () => {
+  const newColor = getRandomHexColor();
+  body.style.backgroundColor = newColor; 
+  colorSpan.textContent = newColor; 
+});
+
+
